888ca05a2db5653d372962b2f160aad3bf20e787
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-11 21:59:36 +0530

    feat(scaffold): add root android build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
+    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.compose.compiler) apply false
+}

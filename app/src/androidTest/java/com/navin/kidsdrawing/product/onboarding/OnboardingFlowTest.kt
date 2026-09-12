67006ee3c285d33a93fb42e34d073cd56a700a94
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-12 17:17:14 +0530

    fix: compile onboarding Compose smoke test

--- app/src/androidTest/java/com/navin/kidsdrawing/product/onboarding/OnboardingFlowTest.kt
+++ app/src/androidTest/java/com/navin/kidsdrawing/product/onboarding/OnboardingFlowTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasSetTextAction
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput

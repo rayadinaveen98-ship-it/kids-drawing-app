701d29e465a12f9e6d6f2a3331e82a8725fbd11a
rayadinaveen98-ship-it <rayadinaveen98@gmail.com>
2026-09-16 12:46:23 +0530

    P6.4 fix Parent accessibility test import

--- app/src/androidTest/java/com/navin/kidsdrawing/product/parent/ParentAccessibilityScreenTest.kt
+++ app/src/androidTest/java/com/navin/kidsdrawing/product/parent/ParentAccessibilityScreenTest.kt
@@ -7,7 +7,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasStateDescription
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNode
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.navin.kidsdrawing.product.accessibility.AccessibilityPreferences
